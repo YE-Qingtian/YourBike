1fc27cd218b62f5ac5281426a158bd4268ccb2ab
Anon <453270758yqt@gmail.com>
2024-02-14 18:16:33 +0000

    first data.py working

---
+++ config.py
@@ -0,0 +1,7 @@
+GoogleMap_api_key = "AIzaSyBVA6mdrqjh6ZXWe5oJBt-Jac8uWOGhDE4"
+JDC_api_key = "7b7c894ed62048d03da1e48c703202765cf2c32e"
+contract_name = "dublin"
+dblink = "mysql://Team24:M49ZiJ4tot2OVUigAZfD@dublinbikedb.cxm60ku2eog6.eu-west-1.rds.amazonaws.com:3306/dublinbike"
+# dblink = "mysql://root:root@127.0.0.1:3306/dublinbike"
+RDSUsername = "Team24"
+RDSPasswd = "M49ZiJ4tot2OVUigAZfD"
---
+++ data.py
@@ -0,0 +1,62 @@
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import insert
+import traceback
+import glob
+import os
+import json
+from pprint import pprint
+import requests
+import time
+import pandas as pd
+from config import *
+
+
+def insert_on_conflict_nothing(table, conn, keys, data_iter):
+    data = [dict(zip(keys, row)) for row in data_iter]
+    stmt = insert(table.table).prefix_with('IGNORE').values(data)
+    result = conn.execute(stmt)
+    return result.rowcount
+
+
+engine = create_engine(dblink)
+metadata = sqla.MetaData()
+station = sqla.Table(
+    "station", metadata,
+    sqla.Column("number", sqla.String(256), nullable=False, primary_key=True),
+    sqla.Column("contract_name", sqla.String(256), nullable=False),
+    sqla.Column("name", sqla.String(256), nullable=False),
+    sqla.Column("address", sqla.String(256), nullable=False),
+    sqla.Column("position_lat", sqla.REAL),
+    sqla.Column("position_lng", sqla.REAL),
+    sqla.Column("banking", sqla.Integer),
+    sqla.Column("bonus", sqla.Integer),
+    sqla.Column("bike_stands", sqla.Integer),
+    sqla.Column("status", sqla.String(256)),
+)
+
+availability = sqla.Table(
+    "availability", metadata,
+    sqla.Column("number", sqla.Integer, primary_key=True, nullable=False),
+    sqla.Column("last_update", sqla.BigInteger, primary_key=True, nullable=False),
+    sqla.Column("available_bike_stands", sqla.Integer),
+    sqla.Column("available_bikes", sqla.Integer),
+)
+
+metadata.create_all(engine)
+
+urlStations = f"https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={JDC_api_key}"
+response = requests.get(urlStations)
+json_struct = json.loads(response.text)
+df = pd.json_normalize(json_struct, sep='_')
+
+dfStations = df[
+    ['number','contract_name', 'name', "address", "position_lat", "position_lng", "banking", "bonus", "bike_stands",
+     "status"]]
+dfAvailability = df[['number', "last_update", 'available_bike_stands', 'available_bikes']]
+try:
+    dfStations.to_sql("station", engine, index=False, if_exists='append')
+except:
+    print("Stations Table Already Exist.")
+
+dfAvailability.to_sql("availability", engine, index=False, if_exists='append', method=insert_on_conflict_nothing)
