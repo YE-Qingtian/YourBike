d3fdc6764aa01c53f387dae8bd20974e1cab7bf2
Anon <453270758yqt@gmail.com>
2024-02-15 09:35:12 +0000

    added dataDownload

--- dataDownload.py
+++ dataDownload.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import requests
 from config import *
 from sqlalchemy import create_engine
-
+import json
 
 engine = create_engine(dblink)
 SQLQuery = """
@@ -9,4 +10,9 @@ SELECT * FROM availability;
 """
 
 dfAvailability = pd.read_sql(SQLQuery, engine)
-dfAvailability['last_update'] = pd.to_datetime(dfAvailability['last_update'],unit="ms")
+dfAvailability['last_update_ymd'] = pd.to_datetime(dfAvailability['last_update'],unit="ms")
+
+
+weatherResponse = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={53}&lon={-6}&dt={1707942432000}&appid={OpenWeather_api_key}")
+
+weather_json = json.loads(weatherResponse.text)
