62b2e8843307b0009466cd517862b6e5d3630abf
Anon <453270758yqt@gmail.com>
2024-02-20 09:59:10 +0000

    Sorted the files, Co-op from now on

--- app.py
+++ app.py
@@ -1,5 +1,53 @@
 """
 This is the main app.
 """
+from flask import Flask, g, render_template, jsonify
+from sqlalchemy import create_engine
+import json
+from config import *
 
-import flask
+app = Flask(__name__, static_url_path='')
+app.config.from_object('config')
+
+
+def connect_to_database():
+    engine = create_engine(dblink)
+    return engine
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+
+# @app.route("/available/<int:station_id>")
+# def get_stations():
+#     engine = get_db()
+#     data = []
+#     rows = engine.execute(f"SELECT available_bikes from stations where number = {station_id};")
+#     for row in rows:
+#         data.append(dict(row))
+#     return jsonify(available=data)
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route('/stations')
+def get_stations():
+    conn = get_db()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
