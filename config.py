32687051c4ccd9d42f438a181a4c5bf2e98755df
EmadNazzal <emadnazzal95@gmail.com>
2024-04-18 15:04:44 +0100

    Remove config.py from repository

--- config.py
+++
@@ -1,9 +0,0 @@
-GoogleMap_api_key = "AIzaSyB0I6hTtpc6uQPVy2wcdKz1ezH4b3QfHlI"
-JDC_api_key = "7b7c894ed62048d03da1e48c703202765cf2c32e"
-OpenWeather_api_key = "6ac1c4119c9a1919193d6b37a98a9b0b"
-
-contract_name = "dublin"
-dblink = "mysql://Team24:M49ZiJ4tot2OVUigAZfD@dublinbikedb.cxm60ku2eog6.eu-west-1.rds.amazonaws.com:3306/dublinbike"
-# dblink = "mysql://root:root@127.0.0.1:3306/dublinbike"
-RDSUsername = "Team24"
-RDSPasswd = "M49ZiJ4tot2OVUigAZfD"
